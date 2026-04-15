c3bf86030f8a2b36c0200ba2be716258813f86db
Andrés Vallejo <andres.vallejo.duque@udla.edu.ec>
2026-04-14 21:22:26 -0500

    Promedio de los estudiantes

--- Vallejo_Andres_Sebastian_Paredes_VectoresyMatrices.c
+++ Vallejo_Andres_Sebastian_Paredes_VectoresyMatrices.c
@@ -16,11 +16,21 @@ int main()
          coprintf("Ingrese la nota %d del estudiante %d(de 0 a 10): ", j + 1, i + 1);
          scanf("%f", &notas[i][j]);
         } while (notas[i][j] < 0 || notas[i][j] > 10);
-        
+
         
       
     }
  }
+ for ( i = 0; i < 5; i++)
+ {
+    suma = 0;
+    for (j = 0; j < 3; j++)
+    {
+        suma += notas[i][j];
+    }
+    promedio[i] = suma / 3;
+ }
+ 
  
     return 0;
 }

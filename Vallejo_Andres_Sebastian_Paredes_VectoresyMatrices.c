93b0f4bf525f2983a5dbeca41397da07fe4f7795
Andrés Vallejo <andres.vallejo.duque@udla.edu.ec>
2026-04-14 21:13:02 -0500

    Ingreso de notas y materias

--- Vallejo_Andres_Sebastian_Paredes_VectoresyMatrices.c
+++ Vallejo_Andres_Sebastian_Paredes_VectoresyMatrices.c
@@ -7,5 +7,14 @@ int main()
  float promedioM[3];
  int i, j;
  float suma;
+ for (i = 0; i < 5; i++)
+ {
+    for (j = 0; j < 3; j++)
+    {
+       printf("Ingrese la nota %d del estudiante %d: ", j + 1, i + 1);
+       scanf("%f", &notas[i][j]);
+    }
+ }
+ 
     return 0;
 }
